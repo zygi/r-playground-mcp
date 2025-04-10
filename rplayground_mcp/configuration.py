247b4d270cd03d202c8becf47b05788c5c309cdc
Zygimantas Straznickas <hi@zygi.me>
2025-04-10 09:47:06 -0700

    Fix Field

--- rplayground_mcp/configuration.py
+++ rplayground_mcp/configuration.py
@@ -1,8 +1,8 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict, Field
-
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
 class Configuration(BaseSettings):    
     model_config = SettingsConfigDict(
         env_nested_delimiter='_', env_nested_max_split=1, env_prefix='RPLAYGROUND_MCP_'
     )
     
-    support_image_output: bool = Field(default=True, env="SUPPORT_IMAGE_OUTPUT")
+    support_image_output: bool = Field(default=True, alias="SUPPORT_IMAGE_OUTPUT")
